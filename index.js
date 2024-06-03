475eed65697980e23e42a90144037d4cae6c4cc1
Davido91390 <114413883+Davido91390@users.noreply.github.com>
2024-06-03 15:43:08 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const userInfo = require('./information');
-
-const cowsay = require ("cowsay");
-
-console.log(cowsay.say( {
-text : `Hi, my name is ${userInfo.name}, I'm ${userInfo.age} from ${userInfo.campus}`
-}));
-
